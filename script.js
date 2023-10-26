68ebc56e26c7ac70bd48ef43797a2c574f241d34
yashsahooXD <yashsahoo04@gmail.com>
2023-10-27 02:09:26 +0530

    added various animation

--- script.js
+++ script.js
@@ -13,5 +13,37 @@ gsap.from(".nlink",{
     opacity: 0,
 })
 
-// shery js to animate images accordingly
+gsap.from(".anim2",{
+    y: 50,
+    stagger: .3,
+    opacity: 0,
+    ease: Expo,
+    duration: 1
+})
 
+Shery.textAnimate("#headings h1", {
+    style: 2,
+    y: 10,
+    delay: 0.3,
+    duration: 2,
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    multiplier: 0.1,
+});
+
+document.querySelector("#ftext")
+.addEventListener("mouseover", function(){
+    gsap.to("#future video",{
+        opacity: 1,
+        duration: 1,
+        ease: Power4
+    })
+})
+
+document.querySelector("#ftext")
+.addEventListener("mouseleave", function(){
+    gsap.to("#future video",{
+        opacity: 0,
+        duration: 1,
+        ease: Power4
+    })
+})
